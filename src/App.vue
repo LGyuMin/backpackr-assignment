25ad92293a397e49090d23e62129d17eabf66e94
규.이규민 <gmlee@lunasoft.co.kr>
2023-10-06 18:19:44 +0900

    글로벌 스타일 수정 및 코드 스타일 수정 입력 폼 UI 렌더링 작업 및 스타일 작업

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
     <div id="app">
-        <h1>Test</h1>
-        <TextForm :text-area-status="'input'" />
+        <h1>백패커 프론트엔드 개발자 포지션 과제</h1>
+
+        <div class="inputFormUi">
+            <h2>입력 폼 UI</h2>
+            <TextForm :text-area-status="'input'" :max-length="500" />
+            <TextForm :text-area-status="'disabled'" :text-area-value="'this is disabled'" :max-length="500" />
+            <TextForm :text-area-status="'readonly'" :text-area-value="'this is readonly'" :max-length="500" />
+        </div>
     </div>
 </template>
 
@@ -16,11 +22,25 @@ export default {
 }
 </script>
 
+<style scoped>
+h1 {
+    margin-bottom: 20px;
+}
+
+.inputFormUi > * {
+    margin-bottom: 15px;
+}
+</style>
+
 <style>
 @import url("https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.5/dist/web/static/pretendard.css");
 
 /* 스타일 초기화 */
-*{margin: 0; padding: 0;}
+*{
+    margin: 0; 
+    padding: 0; 
+    font-family: inherit;
+}
 *, :after, :before { box-sizing: border-box; }
 button {
     background: none;
@@ -36,16 +56,18 @@ button:focus,
 a:focus,
 a:active,
 textarea:focus {
-  outline: 1px solid #dcdcdc;
+    outline: 1px solid #dcdcdc;
 }
 
 #app {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  max-width: 1200px;
-  min-width: 350px;
-  margin: 10px auto;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    max-width: 1200px;
+    min-width: 350px;
+    margin: 10px auto;
+    display: flex;
+    flex-direction: column;
 }
 </style>
