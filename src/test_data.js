85105f7732fcb295fd3d6cc34991046446eef0dd
규.이규민 <gmlee@lunasoft.co.kr>
2023-10-07 18:40:41 +0900

    테스트 데이터 생성

---
+++ src/test_data.js
@@ -0,0 +1,38 @@
+import sample_image from './images/sample_image.png';
+
+const card_test_data = [
+    {
+        cardImageUrl: sample_image,
+        cardImageArt: '샘플 이미지',
+        cardDescObj: {
+            label: '카드 설명 라벨',
+            title: '카드 설명 타이틀',
+            highlight: '카드 설명 하이라이트',
+            crossOut: '카드 설명 취소선'
+        },
+        rate: 4,
+        rateDesc: '별점 설명'
+    },
+    {
+        cardImageUrl: sample_image,
+        cardImageArt: '샘플 이미지',
+        cardDescObj: {
+            label: '카드 설명 라벨',
+            title: '카드 설명 타이틀',
+            highlight: '카드 설명 하이라이트',
+            crossOut: '카드 설명 취소선'
+        }
+    },
+    {
+        cardImageUrl: sample_image,
+        cardImageArt: '샘플 이미지',
+        rate: 4,
+        rateDesc: '별점 설명'
+    },
+    {
+        cardImageUrl: sample_image,
+        cardImageArt: '샘플 이미지'
+    },
+]
+
+export default card_test_data
