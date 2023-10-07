0fe88b7a5217fa81ea1184fc2852660e0ed9beaa
규.이규민 <gmlee@lunasoft.co.kr>
2023-10-07 18:10:51 +0900

    카드 컴포넌트 작업 - prop 정의 - 템플릿 변경 - 스타일 변경

--- src/components/CardComp.vue
+++ src/components/CardComp.vue
@@ -3,26 +3,22 @@
         <div class="cardImgWrapper">
             <img class="cardImg" :src="cardImageUrl" :alt="cardImageArt">
         </div>
-        <div class="cardDesc">
-            <p class="cardDescLabel">Card Label</p>
-            <p class="cardDescTitle">Card Title</p>
-            <p class="cardDescDetail">
-                <span class="cardDetailHighlight">Highlight</span>
-                <span class="cardDetailCrossOut">cross out</span>
-            </p>
+        <div class="cardInfo">
+            <div class="cardDesc">
+                <p class="cardDescLabel ellipsis">Card Label</p>
+                <p class="cardDescTitle ellipsis">Card Title</p>
+                <p class="cardDescDetail">
+                    <span class="cardDetailHighlight ellipsis">Highlight</span>
+                    <span class="cardDetailCrossOut ellipsis">cross out</span>
+                </p>
+            </div>
 
-            <div class="rateWrapper" v-if="isHorizental">
+            <div class="rateWrapper">
                 <div class="rateRow">
                     <span class="rate active" v-for="i of 5" :key="i"></span>
                 </div>
-                <p class="rateDesc">rate desc</p>
+                <p class="rateDesc ellipsis">rate desc</p>
             </div>
-        </div>
-        <div class="rateWrapper" v-if="!isHorizental">
-            <div class="rateRow">
-                <span class="rate active" v-for="i of 5" :key="i"></span>
-            </div>
-            <p class="rateDesc">rate desc</p>
         </div>
     </div>
 </template>
@@ -34,7 +30,22 @@ export default {
         isHorizental: { type:Boolean, required: true },
         cardImageUrl: { type: String, required: true },
         cardImageArt: { type: String, required: true },
-        rate: { type: Number, required: false }
+        cardDescObj: {
+            type: Object,
+            required: false,
+            validator(value){
+                const cardDescKeys = Object.keys(value);
+                return cardDescKeys.indexOf('label') !== -1 && cardDescKeys.indexOf('title') !== -1
+            }
+        },
+        rate: { 
+            type: Number, 
+            required: false,
+            validator(value) {
+                return value > 0 && value % 1 === 0
+            }
+        },
+        rateDesc: { type: String, required: false }
     }
 }
 </script>
@@ -45,11 +56,6 @@ export default {
     flex-direction: column;
     min-width: 20%;
     border: 1px solid #ddd;
-}
-
-.card.horizental {
-    flex-direction: row;
-    min-width: 30%;
 }
 
 .card > div {
@@ -63,12 +69,7 @@ export default {
     overflow: hidden;
 }
 
-.card.horizental .cardImgWrapper {
-    width: 40%;
-    padding-top: 40%;
-}
-
-.card .cardImg {
+.card .cardImgWrapper .cardImg {
     width: 100%;
     height: auto;
     position: absolute;
@@ -76,40 +77,40 @@ export default {
     left: 0;
 }
 
-.card .cardDesc {
-    padding: 5px 10px;
-    width: 100%
+.card .cardInfo {
+    width: 100%;
 }
 
-.card.horizental .cardDesc {
-    width: 60%;
+.card .cardInfo > div {
+    padding: 10px;
 }
 
-.card .cardDesc .cardDescLabel {
+.card .cardInfo .cardDescLabel {
     font-size: 12px;
     color: #b6b6b6;
     margin-bottom: 10px;
 }
 
-.card .cardDesc .cardDescTitle {
+.card .cardInfo .cardDescTitle {
     font-weight: bold;
     margin-bottom: 25px;
 }
 
-.card .cardDesc .cardDescDetail .cardDetailHighlight {
+.card .cardInfo .cardDescDetail .cardDetailHighlight {
     font-size: 12px;
     color: red;
     margin-right: 10px;
 }
 
-.card .cardDesc .cardDescDetail .cardDetailCrossOut {
+.card .cardInfo .cardDescDetail .cardDetailCrossOut {
     font-size: 12px;
     color: #ccc;
     text-decoration: line-through;
 }
 
 .card .rateWrapper {
-    padding: 5px 10px;
+    border-top: 1px solid #ddd;
+    padding-top: 5px;
 }
 
 .rateWrapper .rateRow {
@@ -118,9 +119,9 @@ export default {
 }
 
 .rateRow .rate {
+    display: inline-block;
     width: 15px;
     height: 15px;
-    cursor: pointer;
     background: #ccc;
     border-radius: 50%;
     margin-right: 5px;
@@ -133,5 +134,46 @@ export default {
 .rateWrapper .rateDesc {
     font-size: 12px;
     color: #b6b6b6;
+}
+
+/* 가로 스타일 */
+.card.horizental {
+    flex-direction: row;
+    min-width: 40%;
+}
+
+.card.horizental .cardImgWrapper {
+    width: 40%;
+    padding-top: 40%;
+}
+
+.card.horizental .cardInfo {
+    width: 60%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+
+.card.horizental .cardInfo > div {
+    padding: 0;
+}
+
+.card.horizental .cardInfo .cardDesc {
+    padding: 5px 10px;
+    height: 50%;
+}
+
+.card.horizental .cardInfo .rateWrapper {
+    border-top: none;
+    padding: 5px 10px;
+    height: 30%;
+}
+
+/* 공통 사용 클래스 */
+.ellipsis {
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    word-break: break-all;
 }
 </style>
